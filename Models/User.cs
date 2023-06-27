b60d84f8195d9d9adcbbf70713d2d53e7cb49490
Joseph Streubel <joseph.streubel@gmail.com>
2023-06-26 19:54:48 -0500

    Initial User Model for Back End

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace team_scriptslingers_backend.Models;
+
+public class User{
+    public int userId { get; set; }
+    public string? firstName { get; set; }
+    public string? lastName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string? email { get; set; }
+    [Required]
+    public string? password { get; set; }
+    public string? isAdmin { get; set; }
+    public string? enrolledIn { get; set; }
+}
